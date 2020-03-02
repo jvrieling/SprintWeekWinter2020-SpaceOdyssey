2629d0f9f12baa14b52f48b2031e03d8cbbc54cb
jvrieling <32886119+jvrieling@users.noreply.github.com>
2020-03-02 15:01:56 -0500

    Implement base player movement

---
+++ 2020-Space-Odyssey/Assets/SpaceOdyssey/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private ShipMotor motor;
+    void Awake()
+    {
+        motor = GetComponent<ShipMotor>();
+    }
+
+    
+    void Update()
+    {
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        motor.HandleMovementInput(input);
+    }
+}
