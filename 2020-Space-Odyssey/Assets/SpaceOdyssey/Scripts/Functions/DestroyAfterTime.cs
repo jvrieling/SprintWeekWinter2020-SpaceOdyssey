0e86aa5a750bbf48c56782852a211c730c181633
jvrieling <32886119+jvrieling@users.noreply.github.com>
2020-03-02 16:41:20 -0500

    Implement base shooting, enemies, and explosions

---
+++ 2020-Space-Odyssey/Assets/SpaceOdyssey/Scripts/Functions/DestroyAfterTime.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAfterTime : MonoBehaviour
+{
+    public float timeToDestroy = 0f;
+
+    
+    void Update()
+    {
+        if(timeToDestroy <= 0)
+        {
+            Destroy(gameObject);
+        }
+        timeToDestroy -= Time.deltaTime; 
+    }
+}
---
+++ 2020-Space-Odyssey/Assets/SpaceOdyssey/Scripts/Functions/MoveDownScreen.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveDownScreen : MonoBehaviour
+{
+    public float moveSpeed;
+
+
+    private Rigidbody2D rb;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void Start()
+    {
+        //TODO: Check for modifiers
+    }
+
+    
+    void Update()
+    {
+        transform.Translate(new Vector3(0, -moveSpeed, 0)*Time.deltaTime);
+    }
+}
