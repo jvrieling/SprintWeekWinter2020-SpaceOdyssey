e6c48ff2696e7e8fdd7a71d2903e9728f4fcbf61
jvrieling <32886119+jvrieling@users.noreply.github.com>
2020-03-03 17:36:47 -0500

    Make enemies remove from objectmanager properly when destroyed by player

--- 2020-Space-Odyssey/Assets/SpaceOdyssey/Scripts/Components/Object.cs
+++ 2020-Space-Odyssey/Assets/SpaceOdyssey/Scripts/Components/Object.cs
@@ -6,4 +6,12 @@ public class Object : MonoBehaviour
 {
     public ObjectManager owner;
     public string objectType;
+
+    private void OnDestroy()
+    {
+        if(objectType == "enemy")
+        {
+            owner.RemoveEnemy(GetComponent<MoveDownScreen>());
+        }
+    }
 }
