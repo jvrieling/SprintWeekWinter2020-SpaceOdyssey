75e17d92f0d34b375378feebc90cd04cf27b00b4
TED CRUZUMAKI <SasquatchOverload@gmail.com>
2020-03-03 14:44:11 -0500

    Initial Commit
    Added WaveManager.cs

---
+++ 2020-Space-Odyssey/Assets/SpaceOdyssey/Scripts/Managers/WaveManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* WaveManager.cs
+ * --------------
+ * 
+ * 
+ */
+public class WaveManager : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
