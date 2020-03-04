efc6ee11e754f1d86b93fd5feabe4ff5cb982bfc
arvindavidson <arvindavidson@gmail.com>
2020-03-04 18:53:29 -0500

    twitching joystick

---
+++ 2020-Space-Odyssey/Assets/SpaceOdyssey/Scripts/joystickanim.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class joystickanim : MonoBehaviour
+{
+    private Animator animator;
+    // Start is called before the first frame update
+    public bool P1 = false;
+    public bool P2 = false;
+    void Start()
+    {
+       animator = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (P1 == true)
+        {
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                animator.SetTrigger("Left");
+            }
+            else if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                animator.SetTrigger("Right");
+            }
+        }
+        else if (P2 == true)
+        {
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                animator.SetTrigger("Left");
+            }
+            else if (Input.GetKeyDown(KeyCode.G))
+            {
+                animator.SetTrigger("Right");
+            }
+        }
+    }
+
+}
