f4b49cad59585d3c0ef6e7ab405f58f7524769aa
arvindavidson <arvindavidson@gmail.com>
2020-03-03 19:22:47 -0500

    Sound Working

--- 2020-Space-Odyssey/Assets/SpaceOdyssey/Scripts/Functions/DestroyAfterTime.cs
+++ 2020-Space-Odyssey/Assets/SpaceOdyssey/Scripts/Functions/DestroyAfterTime.cs
@@ -5,18 +5,18 @@ using UnityEngine;
 public class DestroyAfterTime : MonoBehaviour
 {
     public float timeToDestroy = 0f;
-    private AudioSource audioSource;
+    
     private void Start()
     {
-        audioSource = gameObject.GetComponent<AudioSource>();
-        //audioSource.pitch = Random.Range(0.5f, 1f);
-        AudioManager.instance.Play("PlayerDeath");
+        
 
     }
     void Update()
     {
-        if(timeToDestroy <= 0)
+        
+        if (timeToDestroy <= 0)
         {
+            
             Destroy(gameObject);
         }
         timeToDestroy -= Time.deltaTime; 
