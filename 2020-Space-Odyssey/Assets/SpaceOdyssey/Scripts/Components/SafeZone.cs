0b0a479311d6271440db1a47c142b57905a5202b
TED CRUZUMAKI <SasquatchOverload@gmail.com>
2020-03-05 13:26:46 -0500

    Implement Basic Safe Zone
    Looks pretty rough, but has a few tweakable parameters.
    
    No proper sprite yet.

---
+++ 2020-Space-Odyssey/Assets/SpaceOdyssey/Scripts/Components/SafeZone.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SafeZone : MonoBehaviour
+{
+
+    public string[] collisionTags;
+
+    [Header("READ ONLY, DO NOT MODIFY ANY VARIABLE THROUGH INSPECTOR")]
+    public float zoneBaseRadius; //The radius of the zone the moment it spawns.
+    public float zoneMaxRadius; //Max zone radius
+
+    public float zoneDuration; //Time it takes to fully reach the max radius.
+    public float zoneRetainDuration; //Time it lasts after reaching max radius.
+
+
+    [SerializeField]
+    private new CircleCollider2D collider2D;
+
+    private void Start()
+    {
+        if (!collider2D && GetComponent<CircleCollider2D>())
+            collider2D = GetComponent<CircleCollider2D>();
+
+        //collider2D.radius = zoneBaseRadius;
+        transform.localScale = new Vector3(zoneBaseRadius, zoneBaseRadius);
+    }
+
+    private void Update()
+    {
+        ExpandRadius();
+
+        if (zoneRetainDuration <= 0)
+            Destroy(gameObject);
+    }
+
+    void ExpandRadius()
+    {
+        if (transform.localScale.x < zoneMaxRadius)
+        {
+            transform.localScale += new Vector3(zoneMaxRadius / zoneDuration, 
+                                                zoneMaxRadius / zoneDuration) * Time.deltaTime;
+        }
+        else
+            zoneRetainDuration -= Time.deltaTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+
+        //REPLACE DESTROY WITH PROPER INDEX REMOVAL LATER @@@@@@@@@@@@@@@@@@
+        foreach (string i in collisionTags)
+        {
+            if (i == other.gameObject.tag)
+            {
+                Destroy(other.gameObject);
+            }
+        }
+    }
+}
--- 2020-Space-Odyssey/Assets/SpaceOdyssey/Scripts/Managers/BulletModifierManager.cs
+++ 2020-Space-Odyssey/Assets/SpaceOdyssey/Scripts/Managers/BulletModifierManager.cs
@@ -7,7 +7,7 @@ public class BulletModifierManager : MonoBehaviour
 
     public PlayerController playerController;
 
-    public bool canBulletSplit, //After x seconds, your bullets split into 2 by y degrees.
+    public bool canBulletSplit, //After x seconds, your bullets split into y by z degrees.
                 canShotgunShoot; //Fire x more bullets in a cone.
 
     private void Start()
