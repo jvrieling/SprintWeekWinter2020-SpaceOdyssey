e7291b7351b519857bbfb2cc5eff62200cf9b7cc
TED CRUZUMAKI <SasquatchOverload@gmail.com>
2020-03-05 15:19:08 -0500

    WIP Spartan Laser
    Animator-related stuff in progress for the Spartan Laser.

--- 2020-Space-Odyssey/Assets/SpaceOdyssey/Scripts/Managers/BulletModifierManager.cs
+++ 2020-Space-Odyssey/Assets/SpaceOdyssey/Scripts/Managers/BulletModifierManager.cs
@@ -7,12 +7,13 @@ public class BulletModifierManager : MonoBehaviour
 
     public PlayerController playerController;
 
-    public bool canBulletSplit, //After x seconds, your bullets split into y by z degrees.
-                canShotgunShoot; //Fire x more bullets in a cone.
+    public bool canBulletSplit,  //After x seconds, your bullets split into y by z degrees.
+                canShotgunShoot, //Fire x more bullets in a cone.
+                canSpartanLaser; //Activates ALT-FIRE for spartan laser.
 
     private void Start()
     {
-        if (!playerController)
+        if (!playerController && GetComponent<PlayerController>())
             playerController = GetComponent<PlayerController>();
     }
 
